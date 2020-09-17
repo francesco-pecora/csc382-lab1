bf4866c6affbcbd50adc124271d47c714972bb37
francesco-pecora <francescogp9898@hotmail.com>
2020-09-17 16:23:30 -0400

    counting num of operations

--- algorithms/insertionSort.py
+++ algorithms/insertionSort.py
@@ -2,15 +2,40 @@ def insertionSort(arr):
     '''
     Sorting algorithm. Time O(n^2) Space O(1)
     '''
+
+    # initialization of i to 1
+    numSteps = 1
+
     for i in range(1, len(arr)):
+        
+        # boolean comparison, incrementing i by 1
+        numSteps += 2
+
         key = arr[i]
         j = i-1
+
+        # assigning the value of key and the value of j
+        numSteps += 2
+
         while j >=0 and key < arr[j]:
+
+            # two logical comparisons
+            numSteps += 2
+
             arr[j+1] = arr[j]
             j -= 1
+
+            # assignment operaton and decrement j by 1
+            numSteps += 2
+
         arr[j+1] = key
+
+        # assigning the key to the right position
+        numSteps += 1
+
+    return numSteps
 
 if __name__ == '__main__':
     a = [5,9,1,3,4,6,6,3,2]
-    insertionSort(a)
-    print(a)
+    finalSteps = insertionSort(a)
+    print(a, ' -> ', finalSteps)
--- algorithms/mergeSort.py
+++ algorithms/mergeSort.py
@@ -1,36 +1,92 @@
-def mergesort(arr):
+import random
+
+def mergesort(arr, numSteps):
+    '''
+    implementation of merge sort with the
+    addition of counting the number of operations
+    '''
 
     if len(arr) == 1:
-        return arr
+        # counting one comparison in the if statement
+        numSteps += 1
+        return arr, numSteps
+    
     elif len(arr) == 2:
+        # counting one comparison in the if statement
+        numSteps += 1
+        
         if arr[0] > arr[1]:
-            return [arr[1], arr[0]]
+            # counting one comparison in the if statement
+            numSteps += 1
+            return [arr[1], arr[0]], numSteps
         else:
-            return arr
+            # counting one comparison in the if statement (when the if is false, this is immediately run)
+            numSteps += 1
+            return arr, numSteps
 
     p = len(arr)//2
-    m1 = mergesort(arr[:p])
-    m2 = mergesort(arr[p:])
+
+    # counting the assignment of p
+    numSteps += 1
+
+    m1, _ = mergesort(arr[:p], numSteps)
+    m2, _ = mergesort(arr[p:], numSteps)
+
+    # counting the assignments of m1 and m2 
+    numSteps += 1
+    numSteps += 1
 
     result = []
-    while 1:
+
+    # counting the allocation of result
+    numSteps += 1
+
+    while True:
         if len(m1) > 0 and len(m2) > 0:
+
+            # counting the two logical comparisons
+            numSteps += 2
+
             if m1[0] <= m2[0]:
+                # counting the logical comparison
+                numSteps += 1
                 result.append(m1[0])
                 m1 = m1[1:]
+                # counting the assignment of m1 and the update of result
+                numSteps += 2
             else:
+                # counting the logical comparison
+                numSteps += 1
                 result.append(m2[0])
                 m2 = m2[1:]
+                # counting the assignment of m2 and the update of result
+                numSteps += 2
         elif len(m1) > 0:
+            # counting the logical comparisons (the first if and this elif)
+            numSteps += 2
             result += m1
             m1 = []
+            # counting the assignment of m1 and the update of result
+            numSteps += 2
         elif len(m2) > 0:
+            # counting the logical comparisons (the first if, the elif, and the current elif)
+            numSteps += 3
             result += m2
             m2 = []
+            # counting the assignment of m2 and the update of result
+            numSteps += 2
         else:
+            # counting the logical comparisons
+            numSteps += 3
             break
-    return result
+    return result, numSteps
 
 if __name__ == '__main__':
-    a = [5,9,1,3,4,6,6,3,2]
-    print(mergesort(a))
+    '''
+    testing purposes
+    '''
+    n = 100
+    a = [random.randint(1, n) for _ in range(1, n + 1)]
+    a = [x for x in reversed(range(1, n+ 1))]
+    sortedList, steps = mergesort(a, 0)
+    print(sortedList, steps)
